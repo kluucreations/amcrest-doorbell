d729e452af380c1d12b4b69989d4f0df4a74c0ca
Kevin Luu <luukevin87@gmail.com>
2021-12-08 22:20:56 -0800

    init

---
+++ main.py
@@ -0,0 +1,49 @@
+
+from amcrest.http import Http
+import os
+import requests
+
+PUSHOVER_POST_URL = 'https://api.pushover.net/1/messages.json'
+
+def lines(ret):
+    line = ''
+    for char in ret.iter_content(decode_unicode=True):
+        line = line + char
+        if line.endswith('\r\n'):
+            yield line.strip()
+            line = ''
+
+
+def main():
+    pushover_token =  os.getenv('PUSHOVER_TOKEN')
+    pushover_group = os.getenv('PUSHOVER_GROUP')
+
+    host = os.getenv('DOORBELL_HOST')
+    port = os.getenv('DOORBELL_PORT', '80')
+    user = os.getenv('DOORBELL_USER', 'admin')
+    pswd = os.getenv('DOORBELL_PWD')
+    msg = os.getenv('PUSH_MESSAGE', 'Someone is at the Front Door')
+
+    cam = Http(host, port, user, pswd, retries_connection=5, timeout_protocol=3.05)
+    print("Connected")
+    ret = cam.command(
+        'eventManager.cgi?action=attach&codes=[_DoTalkAction_]',
+        timeout_cmd=(3.05, None), stream=True)
+    ret.encoding = 'utf-8'
+
+    try:
+        for line in lines(ret):
+            if "Invite" in line:
+                print("Doorbell Event Received")
+                requests.post(PUSHOVER_POST_URL, data = {
+                    'token': pushover_token,
+                    'user': pushover_group,
+                    'message': msg
+                })
+
+    except KeyboardInterrupt:
+        ret.close()
+        print(' Done!')
+
+if __name__ == '__main__':
+    main()
